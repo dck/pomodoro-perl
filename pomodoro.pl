3b308b418b0de3415807f0d3c52e3d4716efddd2
Denis Usanov <usanovdd@gmail.com>
2013-06-14 02:25:08 +0400

    Mouse, keyboard, touchpad are disabpled. Added supports dual display, codes for devices in `xinput list`

--- pomodoro.pl
+++ pomodoro.pl
@@ -2,11 +2,25 @@
 
 use strict;
 use warnings;
-use constant WORK_TIME => 50 * 60;
-use constant BREAK_TIME => 10 * 60;
+use constant WORK_TIME  => 25 * 60;
+use constant BREAK_TIME =>  6 * 60;
 
-my $toWork = "vbetool dpms on";
-my $toBreak = "vbetool dpms off";
+my $commandsToWork = [
+        "xinput --set-prop 11 \"Device Enabled\" \"1\"",
+        "xinput --set-prop 9 \"Device Enabled\" \"1\"",
+        "xinput --set-prop 15 \"Device Enabled\" \"1\"",
+        "xinput --set-prop 14 \"Device Enabled\" \"1\"",
+        "xset dpms force on"
+    ];
+my $commandsToBreak = [
+        "xinput --set-prop 11 \"Device Enabled\" \"0\"",
+        "xinput --set-prop 9 \"Device Enabled\" \"0\"",
+        "xinput --set-prop 15 \"Device Enabled\" \"0\"",
+        "xinput --set-prop 14 \"Device Enabled\" \"0\"",
+        "xset dpms force off"
+    ];
+# my $toWork = "vbetool dpms on";
+# my $toBreak = "vbetool dpms off";
 my $shift = WORK_TIME;
 my $isWorking = 1;
 
@@ -14,10 +28,16 @@ while( 1 )
 { 
     if ($isWorking) {
         $shift = WORK_TIME;
-        qx{ $toWork };
+        for my $cmd (@$commandsToWork)
+        {
+            qx {$cmd};    
+        }
     } else {
         $shift = BREAK_TIME;
-        qx{ $toBreak };
+        for my $cmd (@$commandsToBreak)
+        {
+            qx {$cmd};    
+        }
     }
     $isWorking = 1 - $isWorking;
     my $now = time;
